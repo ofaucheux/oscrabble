5202a0f090c61a79b9bf7a5b5f55305019b24f1e
olivier <olivier.faucheux@yahoo.de>
2019-11-09 10:53:47 +0100

    typo

--- OScrabble.bat
+++ OScrabble.bat
@@ -1,3 +1,3 @@
 set DEBUG_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044
 set TARGET=C:\Programmierung\OScrabble\build\deployed
-java %DEBUG_OPTS% -jar %TARGET%/scrabble-1.0_BUILD.jar
+java %DEBUG_OPTS% -jar %TARGET%/OScrabble-1.0_BUILD.jar
