c84d32e7c68c8cf840d3389f0cf19a3a26df53f2
olivier <olivier.faucheux@yahoo.de>
2019-11-12 00:15:49 +0100

    Export Config

--- src/main/java/oscrabble/configuration/Configuration.java
+++ src/main/java/oscrabble/configuration/Configuration.java
@@ -1,5 +1,8 @@
 package oscrabble.configuration;
 
+import org.apache.commons.configuration2.HierarchicalConfiguration;
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.tree.ImmutableNode;
 import org.apache.log4j.Logger;
 
 import javax.swing.*;
@@ -107,68 +110,60 @@ public abstract class Configuration
 		panel.removeAll();
 		panel.setLayout(new GridLayout(0, 2));
 
-		for (final Field field : this.getClass().getDeclaredFields())
-		{
-			final Parameter annotation = field.getAnnotation(Parameter.class);
-			if (annotation != null)
-			{
-				field.setAccessible(true);
-				final Object value;
-				try
-				{
-					value = field.get(this);
-				}
-				catch (IllegalAccessException e)
-				{
-					LOGGER.error(e, e);
-					throw new Error("Problem", e);
-				}
-				final JLabel label = new JLabel(annotation.label());
-				label.setToolTipText(annotation.description().isEmpty() ? annotation.label() : annotation.description());
-				panel.add(label);
-				final Class<?> type = field.getType();
-				final Component paramComponent;
-				final PropertyChangeListener listener;
-				if (String.class.equals(type))
-				{
-					paramComponent = new JTextField((String) value);
-					((JTextField) paramComponent).addActionListener(this.listener);  // todo: reicht nicht: Fall berücksichtigen, wo man enter nicht gedrückt hat.
-					listener = evt -> ((JTextField) paramComponent).setText(((String) evt.getNewValue()));
-				}
-				else if (type.isEnum())
-				{
-					final JComboBox<Object> cb = new JComboBox<>(type.getEnumConstants());
-					cb.setSelectedItem(value);
-					paramComponent = cb;
-					cb.addActionListener(this.listener);
-					listener = evt -> cb.setSelectedItem(evt.getNewValue());
-				}
-				else if (boolean.class.equals(type))
-				{
-					final JCheckBox cb = new JCheckBox((String) null, (Boolean) value);
-					paramComponent = cb;
-					cb.setIcon(falseIcon);
-					cb.setSelectedIcon(trueIcon);
-					cb.addActionListener(this.listener);
-					listener = evt -> cb.setSelected(((Boolean) evt.getNewValue()));
-				}
-				else if (int.class.equals(type))
-				{
-					paramComponent = new JSpinner(new SpinnerNumberModel((int) value, 0, Integer.MAX_VALUE, 1));
-					((JSpinner) paramComponent).addChangeListener(this.listener);
-					listener = evt -> ((JSpinner) paramComponent).setValue(((Integer) evt.getNewValue()));
-				}
-				else
-				{
-					throw new IllegalArgumentException("Cannot treat type " + type);
-				}
-				final String fieldName = field.getName();
-				paramComponent.setName(fieldName);
-				this.changeListeners.addPropertyChangeListener(fieldName, listener);
-				panel.add(paramComponent);
-			}
-		}
+		doOnParameters((field, annotation)->addField(panel, field, annotation));
 		return panel;
+	}
+
+	/**
+	 * Fügt zu einem Panel die Widgets für ein Parameter Feld hinzu.
+	 */
+	private void addField(final JPanel panel, final Field field, final Parameter annotation) throws IllegalAccessException
+	{
+		final Object value;
+		value = field.get(this);
+		final JLabel label = new JLabel(annotation.label());
+		label.setToolTipText(annotation.description().isEmpty() ? annotation.label() : annotation.description());
+		panel.add(label);
+		final Class<?> type = field.getType();
+		final Component paramComponent;
+		final PropertyChangeListener listener;
+		if (String.class.equals(type))
+		{
+			paramComponent = new JTextField((String) value);
+			((JTextField) paramComponent).addActionListener(this.listener);  // todo: reicht nicht: Fall berücksichtigen, wo man enter nicht gedrückt hat.
+			listener = evt -> ((JTextField) paramComponent).setText(((String) evt.getNewValue()));
+		}
+		else if (type.isEnum())
+		{
+			final JComboBox<Object> cb = new JComboBox<>(type.getEnumConstants());
+			cb.setSelectedItem(value);
+			paramComponent = cb;
+			cb.addActionListener(this.listener);
+			listener = evt -> cb.setSelectedItem(evt.getNewValue());
+		}
+		else if (boolean.class.equals(type))
+		{
+			final JCheckBox cb = new JCheckBox((String) null, (Boolean) value);
+			paramComponent = cb;
+			cb.setIcon(falseIcon);
+			cb.setSelectedIcon(trueIcon);
+			cb.addActionListener(this.listener);
+			listener = evt -> cb.setSelected(((Boolean) evt.getNewValue()));
+		}
+		else if (int.class.equals(type))
+		{
+			paramComponent = new JSpinner(new SpinnerNumberModel((int) value, 0, Integer.MAX_VALUE, 1));
+			((JSpinner) paramComponent).addChangeListener(this.listener);
+			listener = evt -> ((JSpinner) paramComponent).setValue(((Integer) evt.getNewValue()));
+		}
+		else
+		{
+			throw new IllegalArgumentException("Cannot treat type " + type);
+		}
+		final String fieldName = field.getName();
+		paramComponent.setName(fieldName);
+		this.changeListeners.addPropertyChangeListener(fieldName, listener);
+		panel.add(paramComponent);
 	}
 
 	private abstract static class Listener implements ActionListener, ChangeListener
@@ -193,6 +188,50 @@ public abstract class Configuration
 		{
 			throw new ConfigurationException("Cannot set " + fieldName + " to " + newValue, cause);
 		}
+	}
+
+	HierarchicalConfiguration<ImmutableNode> export()
+	{
+		final XMLConfiguration exportConfig = new XMLConfiguration();
+		doOnParameters((field, anno) -> exportConfig.setProperty(field.getName(), field.get(this)));
+		return exportConfig;
+	}
+
+	void read(HierarchicalConfiguration<ImmutableNode> source)
+	{
+		doOnParameters((field, anno) -> field.set(this, source.get(field.getClass(), field.getName())));
+	}
+
+	/**
+	 * Führt eine Aktion auf alle Felder aus, die mit der Annotation  {@code Parameter} versehen sind.
+	 * @param function die Funktion.
+	 */
+	private void doOnParameters(final FieldConsumer function)
+	{
+		for (final Field field : this.getClass().getDeclaredFields())
+		{
+			final Parameter annotation = field.getAnnotation(Parameter.class);
+			if (annotation != null)
+			{
+				field.setAccessible(true);
+				try
+				{
+					function.accept(field, annotation);
+				}
+				catch (final Throwable e)
+				{
+					final ConfigurationException ex = new ConfigurationException("Error treating field " + field.getName(), e);
+					LOGGER.error(ex, ex);
+					throw ex;
+				}
+			}
+		}
+	}
+
+	@FunctionalInterface
+	interface FieldConsumer
+	{
+		void accept(final Field field, final Parameter annotation) throws IllegalAccessException;
 	}
 
 	/**
