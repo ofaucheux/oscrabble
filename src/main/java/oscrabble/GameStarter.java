432b38003d52ac84c03e3f7034e75608e5e6f37d
olivier <olivier.faucheux@yahoo.de>
2019-12-27 08:41:08 +0100

    Konfiguration lesen

--- src/main/java/oscrabble/GameStarter.java
+++ src/main/java/oscrabble/GameStarter.java
@@ -1,33 +1,83 @@
 package oscrabble;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.log4j.BasicConfigurator;
-import oscrabble.client.SwingPlayer;
+import oscrabble.client.SwingClient;
 import oscrabble.dictionary.Dictionary;
+import oscrabble.player.AbstractPlayer;
 import oscrabble.player.BruteForceMethod;
 import oscrabble.server.Game;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class GameStarter
+public class  GameStarter
 {
 	public static void main(String[] args)
 	{
-		BasicConfigurator.configure();
-		final Game game = new Game(Dictionary.getDictionary(Dictionary.Language.FRENCH));
-
 		try
 		{
-			final File file = new File("C:\\temp\\scrabble.properties");
-			FileUtils.touch(file);
+			final Properties properties = new Properties();
 
-			final Dictionary dictionary = game.getDictionary();
-			final BruteForceMethod method = new BruteForceMethod(dictionary);
-			game.addPlayer(method.new Player("Computer"));
-			game.addPlayer(method.new Player("Computer2"));
-			game.addPlayer(new SwingPlayer("Olivier"));
-			game.addPlayer(new SwingPlayer("Pascal"));
+			final File propertyFile = new File("C:\\temp\\scrabble.properties");
+			try (FileReader reader = new FileReader(propertyFile))
+			{
+				properties.load(reader);
+			}
+
+			final String language = properties.getProperty("LANGUAGE", "FRENCH");
+			Dictionary dictionary;
+			try
+			{
+				dictionary = Dictionary.getDictionary(Dictionary.Language.valueOf(language));
+			}
+			catch (IllegalArgumentException e)
+			{
+				throw new ConfigurationException("Not known language: " + language);
+			}
+			final Game game = new Game(dictionary);
+
+			//
+			// Players
+			//
+
+			final HashMap<String, HashMap<String, String>> players = new HashMap<>();
+			final Pattern keyPattern = Pattern.compile("player\\.([^.]+)\\.(.*)");
+			for (final String key : properties.stringPropertyNames())
+			{
+				final Matcher m = keyPattern.matcher(key);
+				if (m.matches())
+				{
+					final String name = m.group(1);
+					players.putIfAbsent(name, new HashMap<>());
+					players.get(name).put(m.group(2), properties.getProperty(key));
+				}
+			}
+
+			for (Map.Entry<String, HashMap<String, String>> entry : players.entrySet())
+			{
+				String name = entry.getKey();
+				HashMap<String, String> playerProps = entry.getValue();
+				final AbstractPlayer player;
+				final String method = playerProps.get("method");
+				switch (PlayerType.valueOf(method.toUpperCase()))
+				{
+					case SWING:
+						player = new SwingClient(name);
+						break;
+					case BRUTE_FORCE:
+						player = new BruteForceMethod(dictionary).new Player(name);
+						break;
+					default:
+						throw new ConfigurationException("Unknown method: " + method);
+				}
+				game.addPlayer(player);
+			}
+
 			game.startGame();
 		}
 		catch (final Throwable e)
@@ -36,4 +86,27 @@ public class GameStarter
 			JOptionPane.showMessageDialog(null, e, "Error occurred", JOptionPane.ERROR_MESSAGE);
 		}
 	}
+
+	/**
+	 * Problem occurring while reading / writing the configuration.
+	 */
+	private static final class ConfigurationException extends ScrabbleException
+	{
+		/**
+		 * Constructor
+		 * @param message message to display.
+		 */
+		private ConfigurationException(final String message)
+		{
+			super(message);
+		}
+	}
+
+	/** Player types */
+	public enum PlayerType
+	{
+		SWING,
+		BRUTE_FORCE
+	}
+
 }
