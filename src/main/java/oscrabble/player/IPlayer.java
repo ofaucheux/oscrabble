f65ef60052489e3b58d83df56886a4f261c7c553
olivier <olivier.faucheux@yahoo.de>
2020-04-12 22:32:46 +0200

    Interface for players

---
+++ src/main/java/oscrabble/player/IPlayer.java
@@ -0,0 +1,54 @@
+package oscrabble.player;
+
+import oscrabble.configuration.Configuration;
+import oscrabble.server.Server;
+
+import java.util.UUID;
+
+/**
+ * Interface for players.
+ */
+public interface IPlayer extends Server.GameListener
+{
+	/**
+	 * @return the configuration object of this player, {@code null} if no such one.
+	 */
+	Configuration getConfiguration();
+
+	/**
+	 * Set the key generated for this client
+	 * @param key the key
+	 */
+	void setPlayerKey(UUID key);
+
+	/**
+	 * @return name of the player
+	 */
+	String getName();
+
+	/**
+	 * @param server set the game the player will play
+	 */
+	void setGame(Server server);
+
+	/**
+	 *
+	 * @return true if the player doesn't take any active role in the game.
+	 */
+	boolean isObserver();
+
+	/**
+	 * Gibt die Möglichkeit, die Parameters zu ändern. Es kann z.B. durch die Anzeige eines JPanels erfolgen.
+	 */
+	void editParameters();
+
+	/**
+	 * @return type of the player
+	 */
+	Server.PlayerType getType();
+
+	/**
+	 * @return {@code true} wenn manche Parameters editierbar sind.
+	 */
+	boolean hasEditableParameters();
+}
