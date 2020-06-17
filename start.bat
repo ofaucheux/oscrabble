d13b1c4d94c7d8932aabb9bd048d684cc07042fc
olivier <olivier.faucheux@yahoo.de>
2020-06-17 21:16:07 +0200

    Start script

---
+++ start.bat
@@ -0,0 +1,5 @@
+
+start java -jar scrabble-dictionary/build/libs/scrabble-dictionary-1.1.jar
+start java -jar scrabble-server/build/libs/scrabble-server-1.1.jar
+sleep 10
+start java -jar scrabble-client-swing/build/libs/scrabble-client-swing-1.1.jar
