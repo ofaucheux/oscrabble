a0cd188d564d23d409435398808327847d048147
olivier <olivier.faucheux@yahoo.de>
2020-06-17 22:51:25 +0200

    Delay to start the client

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 
 start java -jar scrabble-dictionary/build/libs/scrabble-dictionary-1.1.jar
 start java -jar scrabble-server/build/libs/scrabble-server-1.1.jar
-sleep 10
+timeout /t 10 /nobreak > NUL
 start java -jar scrabble-client-swing/build/libs/scrabble-client-swing-1.1.jar
