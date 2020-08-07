f354c37a3df386502cc7ee38023817d828150ee7
olivier <olivier.faucheux@yahoo.de>
2020-08-07 20:16:36 +0200

    Titles in Dos-Windows

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 
-start java -jar scrabble-dictionary/build/libs/scrabble-dictionary-1.1.jar
-start java -jar scrabble-server/build/libs/scrabble-server-1.1.jar
-timeout /t 10 /nobreak > NUL
-start java -jar scrabble-client-swing/build/libs/scrabble-client-swing-1.1.jar
+start "Dictionary" java -jar scrabble-dictionary/build/libs/scrabble-dictionary-1.1.jar
+start "Server" java -jar scrabble-server/build/libs/scrabble-server-1.1.jar
+timeout /t 5 /nobreak > NUL
+start "Client" java -jar scrabble-client-swing/build/libs/scrabble-client-swing-1.1.jar
