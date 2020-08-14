81febb3b192fb036ed14d19fe8bed5d722e4874d
olivier <olivier.faucheux@yahoo.de>
2020-08-14 20:27:17 +0200

    Neue Version

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 
-start "Dictionary" java -jar scrabble-dictionary/build/libs/scrabble-dictionary-1.1.jar
-start "Server" java -jar scrabble-server/build/libs/scrabble-server-1.1.jar
+start "Dictionary" java -jar scrabble-dictionary/build/libs/scrabble-dictionary-1.2.jar
+start "Server" java -jar scrabble-server/build/libs/scrabble-server-1.2.jar
 timeout /t 5 /nobreak > NUL
-start "Client" java -jar scrabble-client-swing/build/libs/scrabble-client-swing-1.1.jar
+start "Client" java -jar scrabble-client-swing/build/libs/scrabble-client-swing-1.2.jar
