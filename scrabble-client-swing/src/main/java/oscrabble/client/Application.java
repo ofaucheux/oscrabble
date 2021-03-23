a2ede1685ef15f34ca2be6962f865e74d3375412
olivier <olivier.faucheux@yahoo.de>
2021-03-23 20:03:52 +0100

    Number of players

--- scrabble-client-swing/src/main/java/oscrabble/client/Application.java
+++ scrabble-client-swing/src/main/java/oscrabble/client/Application.java
@@ -7,6 +7,8 @@ import oscrabble.controller.MicroServiceScrabbleServer;
 import oscrabble.player.ai.AIPlayer;
 import oscrabble.player.ai.BruteForceMethod;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 @SuppressWarnings("BusyWait")
@@ -14,28 +16,33 @@ public class Application
 {
 	public static final Logger LOGGER = LoggerFactory.getLogger(Thread.class);
 
-	private final MicroServiceDictionary dictionary;
-	private final MicroServiceScrabbleServer server;
-
 	/**
 	 * Resource Bundle
 	 */
 	public final static ResourceBundle MESSAGES = ResourceBundle.getBundle("Messages", new Locale("fr_FR"));
 
-	public Application(final MicroServiceDictionary dictionary, final MicroServiceScrabbleServer server)
+	private final MicroServiceDictionary dictionary;
+	private final MicroServiceScrabbleServer server;
+	private final Properties properties;
+
+	public Application(final MicroServiceDictionary dictionary, final MicroServiceScrabbleServer server, final Properties properties)
 	{
 		this.dictionary = dictionary;
 		this.server = server;
+		this.properties = properties;
 	}
 
-	public static void main(String[] unused) throws InterruptedException
-	{
+	public static void main(String[] unused) throws InterruptedException, IOException {
 
 		Thread.setDefaultUncaughtExceptionHandler((t, e) -> LOGGER.error("Uncaught exception", e));
 
-		final  MicroServiceDictionary dictionary = MicroServiceDictionary.getDefaultFrench();
-		final  MicroServiceScrabbleServer server = MicroServiceScrabbleServer.getLocal();
-		final Application application = new Application(dictionary, server);
+		final MicroServiceDictionary dictionary = MicroServiceDictionary.getDefaultFrench();
+		final MicroServiceScrabbleServer server = MicroServiceScrabbleServer.getLocal();
+		final Properties properties = new Properties();
+		try (final InputStream resource = Application.class.getResourceAsStream("client.properties")) {
+			properties.load(resource);
+		}
+		final Application application = new Application(dictionary, server, properties);
 		application.play();
 	}
 
@@ -48,7 +55,7 @@ public class Application
 
 		final UUID game = this.server.newGame();
 		final UUID edgar = this.server.addPlayer(game, names.get(0));
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < (Integer.parseInt((String)this.properties.get("players.number"))) - 1; i++)
 		{
 			final UUID anton = this.server.addPlayer(game, names.get(i+1));
 			// TODO: tell the server it is an AI Player
