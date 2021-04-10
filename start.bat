03178897c0afe67ea6445f5d290c2e738ec4d619
olivier <olivier.faucheux@yahoo.de>
2021-04-10 15:06:06 +0200

    Commit Configuration file before build

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 
-start "Dictionary" java -jar scrabble-dictionary/build/libs/scrabble-dictionary-1.2.jar
-start "Server" java -jar scrabble-server/build/libs/scrabble-server-1.2.jar
-start "Client" java -jar scrabble-client-swing/build/libs/scrabble-client-swing-1.2.jar
+start "Dictionary" java -jar scrabble-dictionary/build/libs/scrabble-dictionary-*.jar
+start "Server" java -jar scrabble-server/build/libs/scrabble-server-*.jar
+start "Client" java -jar scrabble-client-swing/build/libs/scrabble-client-swing-*.jar
