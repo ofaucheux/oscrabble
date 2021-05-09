d1f589af77b270f06147b12a3c2cf27ea3aaa7b7
olivier <olivier.faucheux@yahoo.de>
2021-05-09 10:36:28 +0200

    SRC-21 Localisation

--- scrabble-client-swing/src/main/java/oscrabble/client/ui/ConnectionParameterPanel.java
+++ scrabble-client-swing/src/main/java/oscrabble/client/ui/ConnectionParameterPanel.java
@@ -7,11 +7,15 @@ import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
+import java.util.ResourceBundle;
 
 /**
  * Panel displaying the values of a {@link oscrabble.client.Application.ConnectionParameters}
  */
 public class ConnectionParameterPanel extends JPanel {
+
+	public final static ResourceBundle MESSAGES = Application.MESSAGES;
+
 	final Application.ConnectionParameters properties;
 	private JTextField server;
 	private JTextField port;
@@ -41,10 +45,10 @@ public class ConnectionParameterPanel extends JPanel {
 
 		final Panel buttonPanel = new Panel(new FlowLayout());
 		final ButtonGroup buttonGroup = new ButtonGroup();
-		final JRadioButton rbLocal = new JRadioButton("Local");
+		final JRadioButton rbLocal = new JRadioButton(MESSAGES.getString("parameter.play.mode.value.local"));
 		buttonGroup.add(rbLocal);
 		buttonPanel.add(rbLocal, gbc);
-		final JRadioButton rbInternet = new JRadioButton("Internet");
+		final JRadioButton rbInternet = new JRadioButton(MESSAGES.getString("parameter.play.mode.choice.internet"));
 		buttonGroup.add(rbInternet);
 		buttonPanel.add(rbInternet, gbc);
 		gbc.gridwidth = GridBagConstraints.REMAINDER;
@@ -55,13 +59,13 @@ public class ConnectionParameterPanel extends JPanel {
 		gbc.gridx = 0;
 		gbc.gridwidth = 1;
 
-		add(new JLabel("Server"), gbc);
+		add(new JLabel(MESSAGES.getString("parameter.label.servername")), gbc);
 		gbc.gridx++;
 		this.server = new JTextField(properties.getServerName());
 		this.server.setColumns(20);
 		add(this.server, gbc);
 		gbc.gridx++;
-		add(new JLabel("Port"), gbc);
+		add(new JLabel(MESSAGES.getString("parameter.labe.serverport")), gbc);
 		gbc.gridx++;
 		this.port = createNumberField(properties.getServerPort());
 		this.port.setColumns(6);
@@ -76,13 +80,9 @@ public class ConnectionParameterPanel extends JPanel {
 			properties.setServerPort(Integer.parseInt(portText.isEmpty() ? "-1" : portText));
 		};
 
-		rbLocal.addActionListener(a -> {
-			updateComponentStatus(true);
-		});
+		rbLocal.addActionListener(a -> updateComponentStatus(true));
 
-		rbInternet.addActionListener(a -> {
-			updateComponentStatus(false);
-		});
+		rbInternet.addActionListener(a -> updateComponentStatus(false));
 
 		this.server.addActionListener(updateProperties);
 		this.port.addActionListener(updateProperties);
