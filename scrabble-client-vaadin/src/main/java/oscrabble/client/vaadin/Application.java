b89ff2ffc7adbd90af48a757ba6b18f64500af73
olivier <olivier.faucheux@yahoo.de>
2022-04-19 13:44:27 +0200

    Vaadin Test, inklusiv automatisch angelegten Dateien

--- scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/Application.java
+++ scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/Application.java
@@ -1,9 +1,13 @@
 package oscrabble.client.vaadin;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
