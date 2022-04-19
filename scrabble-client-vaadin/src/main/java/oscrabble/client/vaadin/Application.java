3f651b49559723904bdb5385df8ecb8991dd7dcb
olivier <olivier.faucheux@yahoo.de>
2022-04-19 07:15:21 +0200

    Start vaadin project

---
+++ scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/Application.java
@@ -0,0 +1,9 @@
+package oscrabble.client.vaadin;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+}
