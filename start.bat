e18e4ebafb1720dfbb3c47cf42009e9b0d9ae3a4
olivier <olivier.faucheux@yahoo.de>
2022-04-19 10:59:01 +0200

    Start script

--- start.bat
+++ start.bat
@@ -1,7 +1,9 @@
 @echo off
 
-for %%y in (scrabble-dictionary scrabble-server scrabble-client-swing) do (
+for %%y in (scrabble-rest-dictionary scrabble-rest-server scrabble-client-swing) do (
     for /f "delims=" %%x in ('dir /od /b %%y\build\libs\*.jar') do (
       start "%%y" java -jar %%y\build\libs\%%x
+      echo "%%y" java -jar %%y\build\libs\%%x
+			pause
     )
 )
