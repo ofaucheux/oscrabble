a9cb303494b718d02f39122f2886e0aa185a0f11
olivier <olivier.faucheux@yahoo.de>
2022-04-20 15:03:05 +0200

    Versuch, den SwingClient zu verwenden, um das Bild zu zeichen. To be continued.

--- scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/ScrabbleView.java
+++ scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/ScrabbleView.java
@@ -4,101 +4,55 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import oscrabble.ScrabbleException;
-import oscrabble.data.ScrabbleRules;
-import oscrabble.data.Tile;
-import oscrabble.data.objects.Grid;
-import oscrabble.data.objects.Square;
+import oscrabble.client.JGrid;
+import oscrabble.data.Action;
+import oscrabble.data.Player;
+import oscrabble.dictionary.Dictionary;
 import oscrabble.dictionary.Language;
-import oscrabble.dictionary.ScrabbleRulesFactory;
+import oscrabble.server.Game;
+import oscrabble.server.Server;
 
-import java.awt.*;
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
 
 @Route(value = "")
 @PageTitle("Scrabble | By Olivier")
 public class ScrabbleView extends VerticalLayout {
-	private static final Color SCRABBLE_GREEN = Color.green.darker().darker();
-	public static final String CELL_SIZE = "30px";
-	private final Grid grid;
 
-	public ScrabbleView() {
-		this.grid = new Grid();
-		try {
-			final ScrabbleRules rules = ScrabbleRulesFactory.create(Language.FRENCH);
-			this.grid.play(rules, "A1 ELEPHANT");
-		} catch (ScrabbleException.NotParsableException e) {
-			throw new Error(e);
-		}
+	private final Game game;
 
+	public ScrabbleView() throws ScrabbleException, InterruptedException {
+		final Server server = new Server();
+		this.game = new Game(server, Dictionary.getDictionary(Language.FRENCH), 2);
+		final UUID eleonore = UUID.randomUUID();
+		this.game.addPlayer(Player.builder().name("Eleonore").id(eleonore).build());
+		this.game.startGame();
+		this.game.play(Action.builder().notation("H8 GATE").player(eleonore).build());
 		addClassName("scrabble-view");
-		setSizeFull();
 		final Div div = new Div();
 		add(div);
 		div.getElement().setProperty("innerHTML", createGridHTML());
 	}
 
-	private static String getHTMLColorString(Color color) {
-		String red = Integer.toHexString(color.getRed());
-		String green = Integer.toHexString(color.getGreen());
-		String blue = Integer.toHexString(color.getBlue());
-
-		return "#" +
-				(red.length() == 1 ? "0" + red : red) +
-				(green.length() == 1 ? "0" + green : green) +
-				(blue.length() == 1 ? "0" + blue : blue);
-	}
-
 	private String createGridHTML() {
-		final CssProperties tableCss = new CssProperties();
-		tableCss.put("background-color", "black");
-		tableCss.put("border-collapse", "collapse");
-
-		final StringBuilder sb = new StringBuilder();
-		sb.append("<table style='").append(tableCss).append("'>");
-		for (int y = 0; y < Grid.GRID_SIZE; y++) {
-			sb.append("<tr>");
-			for (int x = 0; x < Grid.GRID_SIZE; x++) {
-				sb.append("<td>").append(getCellHTML(y+1, x+1));
-			}
+		final JGrid jGrid = new JGrid();
+		jGrid.setGrid(this.game.getGrid());
+		final byte[] image = jGrid.getImage();
+		try {
+			FileUtils.writeByteArrayToFile(new File("C:\\temp\\2022-04-20\\out.png"), image);
+		} catch (IOException e) {
+			throw new IOError(e);
 		}
-		sb.append("</table>");
-		return sb.toString();
+		final String encoded = Base64.getEncoder().encodeToString(image);
+		return String.format(
+				"<img style='display:block' id='base64image' src='data:image/png;base64,%s' />",
+				encoded
+		);
 	}
-
-	private String getCellHTML(final int y, final int x) {
-		final StringBuilder sb = new StringBuilder();
-		final Square square = this.grid.get(x, y);
-
-		final CssProperties cellCss = new CssProperties();
-		final Color cellColor;
-		if (square.tile != null) {
-			cellColor = Color.decode("#FFFFF0").darker(); // ivory
-			cellCss.put("border-radius", "5px");
-		} else if (square.letterBonus == 2) {
-			cellColor = Color.decode("0x00BFFF");
-		} else if (square.letterBonus == 3) {
-			cellColor = Color.blue;
-		} else if (square.wordBonus == 2) {
-			cellColor = Color.decode("#F6CEF5").darker();
-		} else if (square.wordBonus == 3) {
-			cellColor = Color.red;
-		} else {
-			cellColor = SCRABBLE_GREEN;
-		}
-
-		cellCss.put("height", CELL_SIZE);
-		cellCss.put("width", CELL_SIZE);
-		cellCss.put("background-color", getHTMLColorString(cellColor));
-
-		sb.append(String.format(
-				"<div align='center' style='%s'>",
-				cellCss
-			));
-		final Tile tile = square.tile;
-		sb.append(tile != null ? tile.c : ' ');
-		sb.append("</div>");
-		return sb.toString();
-	}
-
-
 }
