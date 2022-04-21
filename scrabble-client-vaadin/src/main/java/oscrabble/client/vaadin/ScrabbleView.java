23a1f1de021179c2a1c2231f5d9393726ea22b4e
olivier <olivier.faucheux@yahoo.de>
2022-04-21 20:58:16 +0200

    SwingClient verwenden, um das Bild zu zeichen.

--- scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/ScrabbleView.java
+++ scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/ScrabbleView.java
@@ -5,7 +5,6 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import oscrabble.ScrabbleException;
 import oscrabble.client.JGrid;
 import oscrabble.data.Action;
@@ -15,16 +14,19 @@ import oscrabble.dictionary.Language;
 import oscrabble.server.Game;
 import oscrabble.server.Server;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.Random;
 import java.util.UUID;
 
 @Route(value = "")
 @PageTitle("Scrabble | By Olivier")
 public class ScrabbleView extends VerticalLayout {
 
+	private static final Random RANDOM = new Random();
 	private final Game game;
 
 	public ScrabbleView() throws ScrabbleException, InterruptedException {
@@ -43,16 +45,27 @@ public class ScrabbleView extends VerticalLayout {
 	private String createGridHTML() {
 		final JGrid jGrid = new JGrid();
 		jGrid.setGrid(this.game.getGrid());
-		final byte[] image = jGrid.getImage();
+
+		final JFrame frame = new JFrame();
+		File file = null;
 		try {
-			FileUtils.writeByteArrayToFile(new File("C:\\temp\\2022-04-20\\out.png"), image);
+			frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			frame.add(jGrid);
+			frame.pack();
+
+			final byte[] image = jGrid.getImage();
+			file = new File(String.format("C:/temp/2022-04-20/out_%s.png", RANDOM.nextLong()));
+			FileUtils.writeByteArrayToFile(file, image);
+			final String encoded = Base64.getEncoder().encodeToString(image);
+			return String.format(
+					"<img style='display:block' id='base64image' src='data:image/png;base64,%s' />",
+					encoded
+			);
 		} catch (IOException e) {
 			throw new IOError(e);
+		} finally {
+			frame.dispose();
+			FileUtils.deleteQuietly(file);
 		}
-		final String encoded = Base64.getEncoder().encodeToString(image);
-		return String.format(
-				"<img style='display:block' id='base64image' src='data:image/png;base64,%s' />",
-				encoded
-		);
 	}
 }
