707aa4090d117837c49061b3636719475202ae9c
olivier <olivier.faucheux@yahoo.de>
2022-04-22 20:19:10 +0200

    SCR-31 Vaadin: complete UI  - columns

--- scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/ScrabbleView.java
+++ scrabble-client-vaadin/src/main/java/oscrabble/client/vaadin/ScrabbleView.java
@@ -1,7 +1,14 @@
 package oscrabble.client.vaadin;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import oscrabble.ScrabbleException;
@@ -9,22 +16,19 @@ import oscrabble.client.JGrid;
 import oscrabble.client.utils.SwingUtils;
 import oscrabble.data.Action;
 import oscrabble.data.Player;
+import oscrabble.data.Score;
 import oscrabble.dictionary.Dictionary;
 import oscrabble.dictionary.Language;
 import oscrabble.server.Game;
 import oscrabble.server.Server;
 
-import javax.swing.border.TitledBorder;
-import java.awt.*;
 import java.util.Base64;
-import java.util.Random;
 import java.util.UUID;
 
-@Route(value = "")
+@Route(value = "scrabble")
 @PageTitle("Scrabble | By Olivier")
-public class ScrabbleView extends VerticalLayout {
+public class ScrabbleView extends HorizontalLayout {
 
-	private static final Random RANDOM = new Random();
 	private final Game game;
 
 	public ScrabbleView() throws ScrabbleException, InterruptedException {
@@ -34,10 +38,33 @@ public class ScrabbleView extends VerticalLayout {
 		this.game.addPlayer(Player.builder().name("Eleonore").id(eleonore).build());
 		this.game.startGame();
 		this.game.play(Action.builder().notation("H8 GATE").player(eleonore).build());
-		addClassName("scrabble-view");
-		final Div div = new Div();
-		add(div);
-		div.getElement().setProperty("innerHTML", createGridHTML());
+
+		//
+		// Center column
+		//
+
+		final VerticalLayout centerColumn = new VerticalLayout();
+		centerColumn.setAlignItems(Alignment.STRETCH);
+
+		final GridComponent grid = new GridComponent();
+		centerColumn.add(grid);
+		centerColumn.add(new TextField());
+
+		add(centerColumn);
+		centerColumn.setWidth(grid.getWidth());
+
+		//
+		// Right column
+		//
+
+		final VerticalLayout rightColumn = new VerticalLayout();
+
+		addTitledComponent(rightColumn, "scores", new PlayerComponent());
+		addTitledComponent(rightColumn, "possible moves", new PossibleMoves());
+		addTitledComponent(rightColumn, "history", new HistoryComponent());
+
+		add(rightColumn);
+		rightColumn.setPadding(false);
 	}
 
 	private String createGridHTML() {
@@ -49,5 +76,34 @@ public class ScrabbleView extends VerticalLayout {
 				"<img style='display:block' id='base64image' src='data:image/png;base64,%s' />",
 				encoded
 		);
+	}
+
+	private void addTitledComponent(final HasComponents parent, final String title, final Component child) {
+		final Element fs = new Element("fieldset");
+		Element legend = new Element("legend").setText(title);
+		fs.appendChild(legend);
+		final Div div = new Div(child);
+		div.setWidth("200px");
+		fs.appendChild(div.getElement());
+		parent.getElement().appendChild(fs);
+	}
+
+	class GridComponent extends Div {
+		GridComponent() {
+			getElement().setProperty("innerHTML", createGridHTML());
+		}
+	}
+
+	class PlayerComponent extends Grid<Player> {
+		PlayerComponent() {
+
+		}
+
+	}
+
+	private class PossibleMoves extends Grid<Score> {
+	}
+
+	private class HistoryComponent extends Grid<Action> {
 	}
 }
