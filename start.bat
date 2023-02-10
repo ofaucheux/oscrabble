92aaed54da762554a2871d7d854d8466051d23df
Olivier <olivier.faucheux@yahoo.de>
2023-02-10 20:44:16 +0100

    SCR-50 Migration von gradle auf maven

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-for %%y in (scrabble-rest-dictionary scrabble-rest-server scrabble-client-swing) do (
-    for /f "delims=" %%x in ('dir /od /b %%y\build\libs\*.jar') do (
-      start "%%y" java -jar %%y\build\libs\%%x
-      echo "%%y" java -jar %%y\build\libs\%%x
-			pause
-    )
-)
--- start_standalone.bat
+++
@@ -1,1 +0,0 @@
-"scrabble-client-swing\build\install\scrabble-client-swing\bin\scrabble-client-swing.bat"
